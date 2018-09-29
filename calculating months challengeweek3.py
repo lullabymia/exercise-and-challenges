3483617621b58162f99916e39e7b99df95c257dd
lullabymia <42403858+lullabymia@users.noreply.github.com>
2018-09-29 10:06:18 +0800

    Update calculating months challengeweek3.py

--- calculating months challengeweek3.py
+++ calculating months challengeweek3.py
@@ -1,17 +1,17 @@
-print('hello')
+
 r = 0.05 / 12
 P = 3000000
 A = 85000
 
 outstanding = P
 
-month = 1
+month = 0
 while outstanding >= 0:
     month = month + 1
     interest = outstanding * r
     principal = A - interest
     outstanding = outstanding - principal
-    row = '{0:04d}	{1:.2f}	{2:.2f}	{3:.2f}	{4:.2f}'.format(month, A, interest, principal, outstanding)
+    row = '{0:04d}	{1:.2f}	{2:7.2f}	{3:.2f}	{4:.2f}'.format(month, A, interest, principal, outstanding)
     print(row)
     if outstanding < 0:
         print('months paid off:', month)
