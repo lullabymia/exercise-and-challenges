01b118b1898399d2be3ea1f1b77d70c5c8529a15
lullabymia <42403858+lullabymia@users.noreply.github.com>
2018-09-30 09:18:49 +0800

    Update week3 hongkong district.py

--- week3 hongkong district.py
+++ week3 hongkong district.py
@@ -1,21 +1,75 @@
 b = {
 
-"Central and Western":	[中西區,244,600],	
-"Eastern":	[東區,574,500],		
-"Southern":	[南區,269,200],	
-"Wan Chai":	[灣仔區,150,900],	
-"Sham Shui Po":	[深水埗區,390,600],	
-"Kowloon City":	[九龍城區,405,400],	
-"Kwun Tong":[觀塘區,641,100],	
-"Wong Tai Sin":	[黃大仙區,426,200],
-"Yau Tsim Mong":[油尖旺區,318,100],	
-"Islands":	[離島區,146,900],
-"Kwai Tsing":	[葵青區,507,100],	
-"North":	[北區,310,800],
-"Sai Kung":	[西貢區,448,600],	
-"Sha Tin":	[沙田區,648,200],	
-"Tai Po":	[大埔區,307,100],	
-"Tsuen Wan":	[荃灣區,303,600],	
-"Tuen Mun":	[屯門區,495,900],	
-"Yuen Long":	[元朗區,607,200],	
+"Central and Western":	[
+  {"name":"中西區",
+  "population":"244,600"}
+   ],	
+"Eastern":	[
+  {"name":"東區",
+  "population":"574,500"}
+   ],		
+"Southern":	[
+  {"name":"南區",
+  "population":269,200"}
+   ],	
+"Wan Chai":	[
+  {"name":"灣仔區",
+   "population":"50,900"}
+],	
+"Sham Shui Po":	[
+  {"name":"深水埗區",
+  "population":"390,600"}
+],	
+"Kowloon City":	[
+  {"name":"九龍城區",
+  "population":"405,400"}
+],	
+"Kwun Tong":[
+  {"name":"觀塘區",
+   "population":"641,100"
+],	
+"Wong Tai Sin":	[
+  {"name":"黃大仙區",
+  "population":"426,200"}
+   ],
+"Yau Tsim Mong":[
+  {"name":"油尖旺區",
+  "population":"318,100"}
+],	
+"Islands":	[
+  {"name":"離島區",
+   "population":"146,900"}
+  ],
+"Kwai Tsing":	[
+  {"name":"葵青區",
+   "population":"507,100"}
+   ],	
+"North":	[
+  {"name":"北區",
+  "population":"310,800"}
+   ],
+"Sai Kung":	[
+  {"name":"西貢區",
+   "population":"448,600"}
+],	
+"Sha Tin":	[
+  {"name":"沙田區",
+   "population":"648,200"}
+],	
+"Tai Po":	[
+  {"name":"大埔區",
+   "population":"307,100"}
+],	
+"Tsuen Wan":	[
+  {"name":"荃灣區",
+    "population":"303,600"}
+],	
+"Tuen Mun":	[
+  {"name":"屯門區",
+   "population":"495,900"}
+],	
+"Yuen Long":	[
+  {"name":"元朗區",
+   "population":"607,200"}
+],	
 }
